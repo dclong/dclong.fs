7839dcf4ec8024f57203effeaf2fe63f820fe225
Chuanlong (Ben) Du <duchuanlong+git@gmail.com>
2013-08-13 00:56:47 -0700

    updated description and change.r

--- dclong.fs/R/change.r
+++ dclong.fs/R/change.r
@@ -12,8 +12,6 @@
 #' @param fpattern a regular expression specifies which files will be changed. 
 #' If NULL, then no file is specified by this argument.
 #' 
-#' @param flist a character vector which specifies a list of files/directories to be changed.
-#' 
 #' @param cfixed logical; Indicate when changing file content, 
 #' file name or directory name, whether regular expression be used or not. 
 #' If true then \code{cpattern} is a string to be matched as it is. 
@@ -59,38 +57,35 @@
 #' fileName = "TEMP_TEMP.TXT"
 #' #write something into the file
 #' cat("Just TRY\n", file = fileName)
-#' #changefc(old = "TRY", new = "try", flist = fileName)
+#' #cfc(old = "TRY", new = "try", fpattern = '*.txt')
 #'
-#' @export changeFileContent changefc changeFileName changefn changeDirName changedn
-#' @importFrom dclong.fs symbolToString
-
-changeFileContent = function(old, new, fpattern = NULL, flist = NULL, cfixed = FALSE, 
-cignore.case = FALSE, fignore.case = TRUE, path = getwd(), dd = NULL, all = TRUE, interact = TRUE, lazy.input = TRUE, ...) {
+#' @export 
+#' @rdname change
+changeFileContent = function(old, new, fpattern = NULL, cfixed = FALSE, 
+cignore.case = FALSE, fignore.case = TRUE, path = getwd(), 
+dd = NULL, all = TRUE, interact = TRUE, lazy.input = TRUE, ...) {
     if(lazy.input){
-        old = dclong.fs::symbolToString(substitute(old))
-        new = dclong.fs::symbolToString(substitute(new))
-        fpattern = dclong.fs::symbolToString(substitute(fpattern))
-        flist = dclong.fs::symbolToString(substitute(flist))
+        old = symbolToString(substitute(old))
+        new = symbolToString(substitute(new))
+        fpattern = symbolToString(substitute(fpattern))
     }
     if(!is.null(dd)){
         path = getDirDef(dd)
     }
     #get the files to be changed
+    files = character()
     if(!is.null(fpattern) && length(fpattern)){
         files = dir(path = path, pattern = fpattern, full.names = TRUE, ignore.case = fignore.case)
-    } else{
-        files = NULL
     }
-    files = c(files, flist)
     files = files[is.file(files, FALSE)]
-    #warn user about the risk of the operation
+    # warn user about the risk of the operation
     if(interact){
         #print out the files to be changed
         cat(paste(files, collapse = "\n"), "\n")
-        cat("File(s) listed above will be changed. Making copies first can avoid unnecessary troubles.\n")
+        cat("Content of files listed above are to be changed. Backup them to avoid problems!\n")
         userIntent = askUserIntent()
         if(userIntent == 'o'){
-          #process one by one      !you'd better make it interactive when changing file content
+          #process one by one      
           return(changeFileContentOneByOne(old, new, files, cfixed, cignore.case, all, ...))
         }
         if(userIntent == 'a'){
@@ -103,110 +98,58 @@ cignore.case = FALSE, fignore.case = TRUE, path = getwd(), dd = NULL, all = TRUE
     #process all file silently
     return(changeMultipleFileContent(old, new, files, cfixed, cignore.case, all, ...))
 }
-
+#' @export
+#' @rdname change
 cfc = changeFileContent
-
-#' @param dpattern a regular expression specifies which directories will be changed. 
-#' If NULL, then no file is specified by this argument.
-#' @param dlist a character vector which specifies a list of directories to be changed.
-changeDirName = function(old, new, dpattern = NULL, dlist = NULL, cfixed = FALSE, cignore.case = TRUE, 
+#' @rdname change
+#' @export
+changeFileName = function(old, new, fpattern = NULL, cfixed = FALSE, cignore.case = TRUE, 
 fignore.case = TRUE, path = getwd(), dd = NULL, all = TRUE, interact = TRUE, lazy.input = TRUE, ...) {
     if(lazy.input){
-        old = dclong.fs::symbolToString(substitute(old))
-        new = dclong.fs::symbolToString(substitute(new))
-        dpattern = dclong.fs::symbolToString(substitute(dpattern))
-        dlist = dclong.fs::symbolToString(substitute(dlist))
-    }
-    if(!is.null(dd)){
-        path = getDirDef(dd)
-    }
-    dirs = NULL
-    if(!is.null(dpattern) && length(dpattern)){
-        dirs = dir(path = path, pattern = dpattern, full.names = TRUE, ignore.case = fignore.case)
-    }
-    dirs = c(dirs, dlist)
-    dirs = dirs[is.dir(dirs, FALSE)]
-    #dir names, parent names and so on
-    if(interact){
-        #check which files are to be changed
-        dirs = grep(pattern = old, x = dirs, ignore.case = cignore.case, value = TRUE, fixed = cfixed, ...)
-        if(length(dirs) == 0){
-            return(cat("No directory names will be changed.\n"))
-        }
-        newDirs = newFileName(old = old, new = new, flist = dirs, cfixed = cfixed, cignore.case = cignore.case, all = all, ...)
-        dirTable = cbind(dirs, newDirs)
-        colnames(dirTable) = c("Old", "New")
-        #print out the directories to be changed
-        print(dirTable)
-        cat("The above renaming will be Done.\n")
-        userIntent = askUserIntent()
-        if(userIntent == 'o'){
-            #process one by one
-            return(changeNameOneByOne(dirTable, "directory", path, cfixed, cignore.case))
-        }
-        if(userIntent == 'a'){
-            #process all files
-            return(changeMultipleName(dirTable, path, cfixed, cignore.case))
-        }
-        #cancel operation
-        return(cancelOperation())
-    }
-    #process all file silently
-    return(changeMultipleName(dirTable, path, cfixed, cignore.case))
-}
-
-cdn = changeDirName
-
-changeFileName = function(old, new, fpattern = NULL, flist = NULL, cfixed = FALSE, cignore.case = TRUE, 
-fignore.case = TRUE, path = getwd(), dd = NULL, all = TRUE, interact = TRUE, lazy.input = TRUE, ...) {
-    if(lazy.input){
-        old = dclong.fs::symbolToString(substitute(old))
-        new = dclong.fs::symbolToString(substitute(new))
-        fpattern = dclong.fs::symbolToString(substitute(fpattern))
-        flist = dclong.fs::symbolToString(substitute(flist))
+        old = symbolToString(substitute(old))
+        new = symbolToString(substitute(new))
+        fpattern = symbolToString(substitute(fpattern))
     }
     if(!is.null(dd)){
         path = getDirDef(dd)
     }
     #get the files
-    files = NULL
+    files = character()
     if(!is.null(fpattern) && length(fpattern)){
         files = dir(path = path, pattern = fpattern, full.names = TRUE, ignore.case = fignore.case)
     }
-    files = c(files, flist)
-    files = files[is.file(files, FALSE)]
     if(interact){
         #check which files are to be changed
-        file.names = dclong.fs::fileName(path = files, extension = TRUE, full = FALSE)
-        file.index = grep(pattern = old, x = file.names, ignore.case = cignore.case, value = FALSE, fixed = cfixed, ...)
-        if(length(file.index) == 0){
-            return(cat("No file names will be changed.\n"))
+        file.names = fileName(path = files, extension = TRUE, full = FALSE)
+        file.names = grep(pattern = old, x = file.names, 
+                          ignore.case = cignore.case, value = TRUE, fixed = cfixed, ...)
+        if(length(file.names) == 0){
+            return(cat("No files to be renamed\n"))
         }
-        newFiles = newFileName(old = old, new = new, flist = file.names, cfixed = cfixed, cignore.case = cignore.case, all = all, ...)
-        parent.folders = dirname(files)
-        fileTable = cbind(files, combinePath(parent.folders, newFiles))
-        colnames(fileTable) = c("Old", "New")
-        #print out the directories to be changed
-        print(fileTable)
-        cat("The above renaming will be Done.\n")
+        newFiles = newFileName(old = old, new = new, files = file.names, 
+            cfixed = cfixed, cignore.case = cignore.case, all = all, ...)
+        ftable = cbind(file.names, newFiles)
+        colnames(ftable) = c("From", "To")
+        #print out the files to be changed
+        print(ftable)
+        cat("The above renaming is to be done.\n")
         userIntent = askUserIntent()
         if(userIntent == 'o'){
             #process one by one
-            return(changeNameOneByOne(old=old, new=new, dlist=fileTable, type="file", path=path, 
-                cfixed=cfixed, cignore.case=cignore.case, all=all))
+            return(changeNameOneByOne(ftable=ftable, path=path)) 
         }
         if(userIntent == 'a'){
             #process all files
-            return(changeMultipleName(old=old, new=new, flist=files, path=path, 
-            cfixed=cfixed, cignore.case=cignore.case, all=all, ...))
+            return(changeMultipleName(ftable=ftable, path=path)) 
         }
         #cancel operation
         cancelOperation()
     }
     #process all file silently
-    return(changeMultipleName(old, new, files, path, cfixed, cignore.case, all, ...))
+    return(changeMultipleName(ftable=ftable, path))
 }
-
+#' @export
+#' @rdname change
 cfn = changeFileName
 
 askUserIntent = function(){
@@ -232,21 +175,43 @@ askUserIntent2 = function(){
     }
 }
 
-changeNameOneByOne = function(old, new, dlist, type, path, cfixed, cignore.case, all, ...){
-    if(length(dlist)){
-        cat(paste("Change the name of the following ", type, "?\n", sep = ""), dlist[1], "\n")
+changeNameOneByOne = function(ftable, path){
+    if(nrow(ftable)){
+        cat("Change \"", ftable[1, 1], "\" to \"", ftable[1, 2], "\"?\n")
         userIntent = askUserIntent2()
         if(userIntent == 'y'){
-            changeOneName(old=old, new=new, flist=dlist[1], path=path, 
-                cfixed=cfixed, cignore.case=cignore.case, all=all, ...)
-            return(changeNameOneByOne(old=old, new=new, dlist=dlist[-1], type=type, path=path, 
+            changeOneName(ftable[1, ,drop=FALSE], path=path)
+            return(changeNameOneByOne(ftable=ftable[-1, ,drop=FALSE], path=path))
+        }
+        if(userIntent == 'a'){
+            return(changeMultipleName(ftable=ftable, path=path))
+        }
+        if(userIntent == 's'){
+            return(changeNameOneByOne(ftable=ftable[-1, ,drop=FALSE], path=path))
+        }
+        if(userIntent == 'p'){
+            return()
+        }
+        cancelOperation()
+    }
+}
+
+changeFileContentOneByOne = function(old, new, files, cfixed, cignore.case, all, ...){
+    if(length(files)){
+        cat("Process the following file?\n", files[1], "\n")
+        userIntent = askUserIntent2()
+        if(userIntent == 'y'){
+            changeOneFileContent(old, new, files[1], cfixed, cignore.case, all, ...)
+            return(changeFileContentOneByOne(old=old, new=new, files=files[-1], 
                 cfixed=cfixed, cignore.case=cignore.case, all=all, ...))
         }
         if(userIntent == 'a'){
-            return(changeMultipleName(old, new, dlist, path, cfixed, cignore.case, all, ...))
+            return(changeMultipleFileContent(old=old, new=new, files=files, 
+                cfixed=cfixed, cignore.case=cignore.case, all=all, ...))
         }
         if(userIntent == 's'){
-            return(changeNameOneByOne(old, new, dlist[-1], type, path, cfixed, cignore.case, all, ...))
+            return(changeFileContentOneByOne(old=old, new=new, files=files[-1], 
+                cfixed=cfixed, cignore.case=cignore.case, all=all, ...))
         }
         if(userIntent == 'p'){
             return()
@@ -255,44 +220,25 @@ changeNameOneByOne = function(old, new, dlist, type, path, cfixed, cignore.case,
     }
 }
 
-changeFileContentOneByOne = function(old, new, flist, cfixed, cignore.case, all, ...){
-    if(length(flist)){
-        cat("Process the following file?\n", flist[1], "\n")
-        userIntent = askUserIntent2()
-        if(userIntent == 'y'){
-            changeOneFileContent(old, new, flist[1], cfixed, cignore.case, all, ...)
-            return(changeFileContentOneByOne(old=old, new=new, flist=flist[-1], 
-                cfixed, cignore.case, all, ...))
-        }
-        if(userIntent == 'a'){
-            return(changeMultipleFileContent(old, new, flist, cfixed, cignore.case, all, ...))
-        }
-        if(userIntent == 's'){
-            return(changeFileContentOneByOne(old, new, flist[-1], cfixed, cignore.case, all, ...))
-        }
-        if(userIntent == 'p'){
-            return()
-        }
-        cancelOperation()
-    }
-}
-
-changeOneFileContent = function(old, new, flist, cfixed, cignore.case, all, ...){
+changeOneFileContent = function(old, new, file, cfixed, cignore.case, all, ...){
     #read the file content
-    fileContent = readText(file = flist, lazy.input = FALSE)
+    fileContent = readText(file = file, lazy.input = FALSE)
     #replace old string with new string
     if(all){
-        fileContent = gsub(pattern = old, replacement = new, fileContent, ignore.case = cignore.case, fixed = cfixed, ...)
+        fileContent = gsub(pattern = old, replacement = new, x=fileContent, 
+            ignore.case = cignore.case, fixed = cfixed, ...)
     }else{
-        fileContent = sub(pattern = old, replacement = new, fileContent, ignore.case = cignore.case, fixed = cfixed, ...)
+        fileContent = sub(pattern = old, replacement = new, x=fileContent, 
+            ignore.case = cignore.case, fixed = cfixed, ...)
     }
     #rewrite the file
-    cat(fileContent, file = flist, append = FALSE)
+    cat(fileContent, file = file, append = FALSE)
 }
 
-changeMultipleFileContent = function(old, new, flist, cfixed, cignore.case, all, ...){
-    for(file in flist){
-        changeOneFileContent(old, new, file, cfixed, cignore.case, all, ...)
+changeMultipleFileContent = function(old, new, files, cfixed, cignore.case, all, ...){
+    for(file in files){
+        changeOneFileContent(old = old, new = new, file = file, 
+             cfixed = cfixed, cignore.case = cignore.case, all = all, ...)
     }
 }
 
@@ -300,24 +246,24 @@ cancelOperation = function(){
     stop("operation is canceled by user.")
 }
 
-newFileName = function(old, new, flist, cfixed, cignore.case, all, ...){
+newFileName = function(old, new, files, cfixed, cignore.case, all, ...){
     if(all){
-        return(gsub(pattern = old, replacement = new, x = flist, ignore.case = cignore.case, fixed = cfixed, ...))
+        return(gsub(pattern = old, replacement = new, x = files, ignore.case = cignore.case, fixed = cfixed, ...))
     }
-    return(sub(pattern = old, replacement = new, x = flist, ignore.case = cignore.case, fixed = cfixed, ...))
+    return(sub(pattern = old, replacement = new, x = files, ignore.case = cignore.case, fixed = cfixed, ...))
 }
 
-changeOneName = function(old, new, flist, path, cfixed, cignore.case, all, ...){
-    newName = newFileName(old = old, new = new, flist = flist, 
-        cfixed = cfixed, cignore.case = cignore.case, all = all, ...)
-    newFullPath = combinePath(path, newName)
-    oldFullPath = combinePath(path, flist)
-    cat("Renaming \n", oldFullPath, "to \n", newFullPath)
-    file.rename(from = oldFullPath, to = newFullPath)
+changeOneName = function(files, path){
+    from = combinePath(path, files[1])
+    to = combinePath(path, files[2])
+    file.rename(from = from, to = to)
 }
 
-changeMultipleName = function(old, new, flist, path, cfixed, cignore.case, all, ...){
-    for(f in flist){
-        changeOneName(old, new, f, path, cfixed, cignore.case, all, ...)
+changeMultipleName = function(ftable, path){
+    n = nrow(ftable)
+    i = 1
+    while(i <= n){
+        changeOneName(ftable[i, ], path)
+        i = i + 1
     }
 }
